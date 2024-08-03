ee4d7a3d6438480ebb9b075df2690f1987f83084
CarlosMateoM <carlosmateo484@gmail.com>
2024-08-03 16:19:47 -0500

    change: sport center jpa model

--- src/main/java/cancha/directa/model/SportsCenter.java
+++ src/main/java/cancha/directa/model/SportsCenter.java
@@ -1,20 +1,35 @@
 package cancha.directa.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
-@Entity
+@Entity (name = "SportCenter")
+@Table(name = "sports_centers")
 public class SportsCenter {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(
+            nullable = false
+    )
     private String address;
+
+    @Column(
+            unique = true,
+            nullable = false
+    )
     private String phone;
 }
