0a89a2b48c7626de99fab66fa02ae36abb4310a6
CarlosMateoM <carlosmateo484@gmail.com>
2024-08-03 08:43:01 -0500

    first commit

---
+++ src/main/java/cancha/directa/CanchaDirectaApplication.java
@@ -0,0 +1,13 @@
+package cancha.directa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanchaDirectaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanchaDirectaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cancha/directa/ApiReservaDeCanchasDeportivasApplicationTests.java
@@ -0,0 +1,13 @@
+package cancha.directa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiReservaDeCanchasDeportivasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
